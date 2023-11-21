d25dedec3e7536023e5a5f119eabf602db02b500
jhenacerarellano <148493533+jhenacerarellano@users.noreply.github.com>
2023-11-21 08:42:44 -0600

    Add files via upload

---
+++ retocondicionales.js
@@ -0,0 +1,35 @@
+let usuario = {
+    "edad": 26,
+};
+ if (usuario.edad >=18){
+    console.log("eres mayor de edad.");
+ }
+else {
+    console.log("eres menor de edad");
+}
+
+let accion= "crear";
+switch(accion){
+    case "crear":
+          console.log("la acción es crear");
+          break
+    case "anyone":
+           console.log("es otra la acción");
+          break
+    default:
+        ("lo siento, no conseguimos la acción");
+
+}
+
+let numero = 1;
+
+ if (numero % 2 === 0){
+    console.log("el numero ingresado es par");
+ }
+ else{
+    console.log("el numero ingresado es impar");
+ }
+
+
+
+ 
